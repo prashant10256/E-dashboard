012d6f1b17f6807f8855429c3dbe3fb6e61345f0
Your Name <prashantsugave125@gmail.com>
2024-09-01 22:11:09 +0530

    initial commit

---
+++ front-end/src/components/Footer.js
@@ -0,0 +1,11 @@
+import react from "react"
+
+const Footer = () =>{
+    return(
+        <div className="footer">
+            <h3>E-com Dashboard</h3>
+        </div>
+    )
+}
+
+export default Footer;
